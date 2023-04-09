30819513ee34a494cc26768ad121740b5c27bd48
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-04-09 16:43:44 -0600

    Repaso del proyecto
    Reacomodé parte del código en la historia creada por Angela, y repasé la funcionalidad del proyecto.

--- Destini/Model/StoryBrain.swift
+++ Destini/Model/StoryBrain.swift
@@ -6,12 +6,35 @@ struct StoryBrain {
     /* Atributos */
     
     // Arreglo de objetos tipo Story para la app
-    let dataBase = [    Story(title: "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: 'Need a ride, boy?'.", choice1: "I'll hop in. Thanks for the help!", choice1Destination: 2, choice2: "Better ask him if he's a murderer first.", choice2Destination: 1),
-                        Story(title: "He nods slowly, unfazed by the question.", choice1: "At least he's honest. I'll climb in.", choice1Destination: 2, choice2: "Wait, I know how to change a tire.", choice2Destination: 3),
-                        Story(title: "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.", choice1: "I love Elton John! Hand him the cassette tape.", choice1Destination: 5, choice2: "It's him or me! You take the knife and stab him.", choice2Destination: 4),
-                        Story(title: "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?", choice1: "The", choice1Destination: 0, choice2: "End", choice2Destination: 0),
-                        Story(title: "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.", choice1: "The", choice1Destination: 0, choice2: "End", choice2Destination: 0),
-                        Story(title: "You bond with the murderer while crooning verses of 'Can you feel the love tonight'. He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: 'Try the pier.'", choice1: "The", choice1Destination: 0, choice2: "End", choice2Destination: 0) ]
+    let dataBase = [
+        Story(title: "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: 'Need a ride, boy?'.", choice1: "I'll hop in. Thanks for the help!",
+            choice1Destination: 2,
+            choice2: "Better ask him if he's a murderer first.",
+            choice2Destination: 1),
+        Story(title: "He nods slowly, unfazed by the question.",
+            choice1: "At least he's honest. I'll climb in.",
+            choice1Destination: 2,
+            choice2: "Wait, I know how to change a tire.",
+            choice2Destination: 3),
+        Story(title: "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.", choice1: "I love Elton John! Hand him the cassette tape.",
+            choice1Destination: 5,
+            choice2: "It's him or me! You take the knife and stab him.",
+            choice2Destination: 4),
+        Story(title: "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?",
+            choice1: "The",
+            choice1Destination: 0,
+            choice2: "End",
+            choice2Destination: 0),
+        Story(title: "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.",
+            choice1: "The",
+            choice1Destination: 0,
+            choice2: "End",
+            choice2Destination: 0),
+        Story(title: "You bond with the murderer while crooning verses of 'Can you feel the love tonight'. He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: 'Try the pier.'",
+            choice1: "The",
+            choice1Destination: 0,
+            choice2: "End",
+            choice2Destination: 0) ]
     
     // Variable que controla la historia mostrada.
     var storyNumber = 0
@@ -37,7 +60,7 @@ struct StoryBrain {
     mutating func nextStory(_ userChoise: String) {
         // Guardamos el objeto actual del arreglo en una variable sencilla para utilizarla varias veces y simplificar el código.
         let currentStory = dataBase[storyNumber]
-        // A partir de la "histori actual", vamos a comparar la variable userChoice que viene definida por el current.senderTitle! del botón que se presionó.
+        // A partir de la "historia actual", vamos a comparar la variable userChoice que viene definida por el current.senderTitle! del botón que se presionó.
         if(userChoise == currentStory.choice1) {
             storyNumber = currentStory.choice1Destination
         }
